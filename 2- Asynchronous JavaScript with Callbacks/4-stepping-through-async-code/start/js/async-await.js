9d145a16af000f99ca3d08761cac7f706453342d
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-07-28 20:05:07 -0600

    Setting up to catch errors

--- 2- Asynchronous JavaScript with Callbacks/4-stepping-through-async-code/start/js/async-await.js
+++ 2- Asynchronous JavaScript with Callbacks/4-stepping-through-async-code/start/js/async-await.js
@@ -4,20 +4,29 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // Handle all fetch requests
+async function getJSON(url) {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch(error) {
+    throw error;
+  }
+}
+
 async function getPeopleInSpace(url) {
-  const peopleResponse = await fetch(url);
-  const peopleJSON = await peopleResponse.json();
+  const peopleJSON = await getJSON(url);
 
   const profiles = peopleJSON.people.map(async (person) => {
     const craft = person.craft;
-    const profileResponse = await fetch(wikiUrl + person.name);
-    const profileJSON = await profileResponse.json();
+    const profileJSON = await getJSON(wikiUrl + person.name);
 
     return {...profileJSON, craft};
   });
 
   return Promise.all(profiles);
 }
+
+
 
 // Generate the markup for each profile
 function generateHTML(data) {
@@ -33,10 +42,14 @@ function generateHTML(data) {
   });
 }
 
-btn.addEventListener('click', async (event) => {
+btn.addEventListener('click', (event) => {
   event.target.textContent = "Loading...";
 
-  const astors = await getPeopleInSpace(astrosUrl);
-  generateHTML(astors);
-  event.target.remove();
+  getPeopleInSpace(astrosUrl)
+    .then(generateHTML)
+    .catch(e => {
+      peopleList.innerHTML = `<h3>Something went wrong!</h3>`;
+      console.log(e);
+    })
+    .finally(() => event.target.remove());
 });
